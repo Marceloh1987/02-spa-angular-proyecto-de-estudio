f55cf67f22af84d89d3f9d7348db32d40890d5d6
Marceloh1987 <55755584+Marceloh1987@users.noreply.github.com>
2020-09-14 13:18:55 -0300

    Proyecto de estudio angular

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../service/heroes.service';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+  heroe: any = {};
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _heroesService: HeroesService
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.heroe = this._heroesService.getheroe(params['id']);
+      console.log(this.heroe)
+    });
+  }
+}
